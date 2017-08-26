2f5c9fcd7deb3c1ff77aee62e2c70758a62bc4ea
narekye <yegoryan.narek@gmail.com>
2017-08-26 00:48:12 -0600

    constants added

---
+++ gAutomotive/gAutomoive.LocalClient/Constants/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gAutomoive.LocalClient.Constants
+{
+    public class Constants
+    {
+        public const string BaseUrl = "http://gautomotive.azurewebsites.net";
+    }
+}

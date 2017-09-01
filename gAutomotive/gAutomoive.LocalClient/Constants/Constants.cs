31d9919044ead5d7d9a534278b63ac9a88154f5f
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-09-01 15:48:11 +0400

    commit

--- gAutomotive/gAutomoive.LocalClient/Constants/Constants.cs
+++ gAutomotive/gAutomoive.LocalClient/Constants/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace gAutomoive.LocalClient.Constants
+﻿namespace gAutomoive.LocalClient.Constants
 {
     public class Constants
     {

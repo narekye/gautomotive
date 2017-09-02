9cf83950408082e367136cd3cc9df7779a576e3a
narekye <yegoryan.narek@gmail.com>
2017-09-02 21:40:53 +0400

    commit

--- gAutomotive/gAutomoive.LocalClient/MainWindow.xaml.cs
+++ gAutomotive/gAutomoive.LocalClient/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using gAutomotive.DAL;
 using gAutomotive.DAL.Repository;
 
@@ -39,7 +30,7 @@ namespace gAutomoive.LocalClient
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            var obj = this.phonetext.Text;
+            var obj = phonetext.Text;
             IEnumerable<Customer> query = this.customers.ToList();
             if (obj != null)
             {
